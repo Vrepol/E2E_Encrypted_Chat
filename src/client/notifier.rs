d867620be875500a9f6bb95f58c910de503a9dbd
pi <1291353855@qq.com>
2025-05-07 20:36:53 +0800

    可能增加了消息提醒

--- src/client/notifier.rs
+++ src/client/notifier.rs
@@ -1,21 +1,23 @@
-// src/client/notifier.rs
-#[cfg(target_os = "windows")]
+use windows::Win32::{
+    System::Console::GetConsoleWindow,
+    UI::WindowsAndMessaging::{
+        FlashWindowEx,
+        FLASHWINFO, FLASHW_ALL,
+    },
+};
 pub fn notify() {
-    // 让任务栏图标闪烁 3 次，相当于 微信的“消息提醒”
-    use windows::Win32::{
-        System::Console::GetConsoleWindow,
-        UI::WindowsAndMessaging::{FlashWindowEx, FLASHWINFO, FLASHW_ALL},
-    };
     unsafe {
         let hwnd = GetConsoleWindow();
-        if !hwnd.is_invalid() {
-            let mut info = FLASHWINFO::default();
-            info.cbSize    = std::mem::size_of::<FLASHWINFO>() as u32;
-            info.hwnd      = hwnd;
-            info.dwFlags   = FLASHW_ALL;
-            info.uCount    = 3;
-            info.dwTimeout = 0;
-            let _ = FlashWindowEx(&info);
-        }
+        if hwnd.0 != 0 {
+            let mut info = FLASHWINFO {
+                cbSize: std::mem::size_of::<FLASHWINFO>() as u32,
+                hwnd,
+                dwFlags: FLASHW_ALL,
+                uCount: 3,
+                dwTimeout: 0,
+            };
+            FlashWindowEx(&mut info);          // 任务栏闪
+        }        // 统一级“叮”声
     }
 }
+
